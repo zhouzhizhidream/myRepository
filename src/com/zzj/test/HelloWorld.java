076218fa09ce579aaae5a49986e47b36da8bcc1e
zhouzhijian <546340711@qq.com>
2019-07-04 12:17:35 +0800

    增加打印代码

--- src/com/zzj/test/HelloWorld.java
+++ src/com/zzj/test/HelloWorld.java
@@ -11,5 +11,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world!");
         System.out.println("zzj");
+        System.out.println("heool");
     }
 }
