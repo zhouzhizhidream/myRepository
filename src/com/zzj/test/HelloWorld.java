3167c7c19ad4482b4639c3e9b449932518ea8faf
zhouzhizhidream <30515165+zhouzhizhidream@users.noreply.github.com>
2019-07-04 11:45:46 +0800

    Update HelloWorld.java
    增加打印字段

--- src/com/zzj/test/HelloWorld.java
+++ src/com/zzj/test/HelloWorld.java
@@ -10,5 +10,6 @@ package com.zzj.test;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world!");
+        System.out.println("zzj");
     }
 }
