01b008704cfe54af781eda591b0d76e4ca2c30c9
zhouzhijian <546340711@qq.com>
2019-07-04 11:39:31 +0800

    测试链接

---
+++ src/com/zzj/test/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.zzj.test;
+
+/**
+ * @PackageName: com.zzj.test
+ * @Description: helloworld
+ * @ClassName: HelloWorld
+ * @Author: zhouzhijian
+ * @Date: 2019/7/4 11:32
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
